475ae05d2c3c0e15a50faa9fc8beb1d31bbdae22
Karsten Silz <karsten.silz@outlook.com>
2023-03-18 16:26:02 +0000

    Initial commit
    Signed-off-by: Karsten Silz <karsten.silz@outlook.com>

---
+++ src/main/java/com/betterprojectsfaster/getstarted/nativejava/spring2/NativeJavaAllThumbsSpring2Application.java
@@ -0,0 +1,13 @@
+package com.betterprojectsfaster.getstarted.nativejava.spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeJavaAllThumbsSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeJavaAllThumbsSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/betterprojectsfaster/getstarted/nativejava/spring2/NativeJavaAllThumbsSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterprojectsfaster.getstarted.nativejava.spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeJavaAllThumbsSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
